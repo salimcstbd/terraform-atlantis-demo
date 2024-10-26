86114e8f9121f3251e0f8bc28c59533c958c46da
salimcstbd <160099764+salimcstbd@users.noreply.github.com>
2024-10-26 17:52:03 +0600

    Update main.tf

--- myproject/main.tf
+++ myproject/main.tf
@@ -1,1 +1,38 @@
-resource "null_resource" "demo" {}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    env = "development"
+  }
+}
